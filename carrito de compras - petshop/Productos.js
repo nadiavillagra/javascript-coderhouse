a407f4bbfe74054574cb357b4a025c9aedfd9b09
nadiavillagra <nadiavillagra@yahoo.com>
2022-11-12 23:58:00 -0300

    segunda entrega

---
+++ carrito de compras - petshop/Productos.js
@@ -0,0 +1,8 @@
+class Productos {
+    constructor (id, nombre, peso, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.peso = peso;
+        this.precio = Number(precio);
+    }
+}
--- carrito de compras - petshop/main.js
+++ carrito de compras - petshop/main.js
@@ -1,44 +1,100 @@
+const arrayProductos = [];
+
+const producto1 = new Productos(1, 'alimento para cachorros', '21 kg', 7361)
+const producto2 = new Productos(2, 'alimento para adultos carne y vegetales', '21 kg', 6894)
+const producto3 = new Productos(3, 'alimento para adultos carne, pollo y cereales', '21 kg', 6893)
+const producto4 = new Productos(4, 'alimento para razas pequeñas', '15 kg', 6625)
+const producto5 = new Productos(5, 'alimento para senior', '8 kg', 3630)
+const producto6 = new Productos(6, 'alimento para gatitos', '10 kg', 6001)
+const producto7 = new Productos(7, 'alimento para gato carne', '10.1 kg', 5355)
+const producto8 = new Productos(8, 'alimento para gato pollo', '10.1 kg', 5355)
+const producto9 = new Productos(9, 'alimento para gato pescado', '10.1 kg', 5355)
+const producto10 = new Productos(10, 'alimento para gato hogareño', '10 kg', 6001)
+
+arrayProductos.push(producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10)
+
+// funcion para ordenar valores de menor a mayor
+const valorMenorMayor = () => {
+    arrayProductos.sort((a, b) => a.precio - b.precio);
+    mostrarListaOrdenada();
+};
+
+// funcion para ordenar valores de mayor a menor
+const valorMayorMenor = () => {
+    arrayProductos.sort((a, b) => b.precio - a.precio);
+    mostrarListaOrdenada();
+};
+
+const mostrarListaOrdenada = () => {
+    let array = [];
+    arrayProductos.forEach(producto => array.push(producto.nombre+' $'+producto.precio));
+    alert('lista de precios'+'\n'+array.join('\n'))
+};
+
 function comprarProdutos() {
-    let producto = '';
-    let precio = 0;
-    let cantidad = 0;
-    let totalCompra = 0;
-    let cantidadTotal = 0;
+    let productoNombre = '';
+    let productoCantidad = 0;
+    let total = 0;
     let seguirComprando = false;
 
     do {
-        producto = prompt('¿Queres comprar alimento para perros, gatos o ambos?', 'Ej: ambos');
-        cantidad = parseInt(prompt('¿Cuantos queres comprar?'));
+        productoNombre = prompt('¿Cual elemento de la lista queres comprar?', 'Ej: limento para razas pequeñas');
+        productoCantidad = parseInt(prompt('¿Cuantos queres comprar?'));
 
-        let cantidadValidada = validarCantidad(cantidad);
+        const producto = arrayProductos.find(producto => producto.nombre === productoNombre);
 
-        switch (producto) {
-            case 'perros':
-                precio = 500;
-                break;
-            case 'gatos':
-                precio = 600;
-                break;
-            case 'ambos':
-                precio = 1100;
-                break;
-            default:
-                alert('Alguno de los datos ingresados no es correcto');
-                precio = 0;
-                cantidad = 0;
-                break;
+        if (producto){
+            total += producto.precio * productoCantidad;
+        } else {
+            alert('El producto no se encuentra disponible.');
         }
-
-        totalCompra += precio * cantidadValidada;
-        cantidadTotal += cantidad;
 
         seguirComprando = confirm('¿Queres agregar otro producto?');
 
     } while (seguirComprando)
 
-    return totalCompra;
+    descuentoCompra(total);
 }
 
+function descuentoCompra(totalCompra) {
+    const descuento = 0.80;
+    if (totalCompra >= 1000) {
+        totalCompra = totalCompra * descuento;
+        alert('Tenes 20% de descuento')
+    }
+    calcularEnvio(totalCompra)
+}
+
+function calcularEnvio(totalCompra) {
+    const costoEnvio = 500;
+    let envioADomicilio = confirm('Quieres envio a domicilio?');
+
+    if (envioADomicilio && totalCompra >= 2000) {
+        alert('Tienes envio gratis. El total de la compra es: ' + totalCompra);
+    } else if (envioADomicilio && totalCompra < 2000 && totalCompra !== 0) {
+        totalCompra += costoEnvio;
+        alert('El costo del envio es de $500. El total de la compra es: ' + totalCompra);
+    } else {
+        alert('El total de la compra es: ' + totalCompra);
+    }
+};
+
+function comprar() {
+    const quieroOrdenarla =confirm('¿Queres ordenar la lista de menor valor al mayor?');
+    if(quieroOrdenarla) {
+        valorMenorMayor();
+    }else {
+        valorMayorMenor();
+    }
+
+    comprarProdutos();
+}
+
+comprar();
+
+
+
+/* 
 function validarCantidad(cantidad) {
     while (Number.isNaN(cantidad) || cantidad === 0) {
         if (cantidad !== 0) {
@@ -67,13 +123,13 @@ function calcularEnvio(totalCompra) {
     let envioADomicilio = confirm('Quieres envio a domicilio?');
 
     if (envioADomicilio && totalCompra >= 2000) {
-        alert('Tienes envio gratis. El total de la compra es: $' + totalCompra);
+        alert('Tienes envio gratis. El total de la compra es: ' + totalCompra);
     } else if (envioADomicilio && totalCompra < 2000 && totalCompra !== 0) {
         totalCompra += costoEnvio;
-        alert('El costo del envio es de $500. El total de la compra es: $' + totalCompra);
+        alert('El costo del envio es de $500. El total de la compra es: ' + totalCompra);
     } else {
-        alert('El total de la compra es: $' + totalCompra);
+        alert('El total de la compra es: ' + totalCompra);
     }
 }
 
-calcularEnvio(descuentoCompra(comprarProdutos()));
+calcularEnvio(descuentoCompra(comprarProdutos())); */
